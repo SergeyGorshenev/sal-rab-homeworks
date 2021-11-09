f80c23c7431bcad59d3469a1cbf4b57259a78cb5
Sergey Gorshenev <gors1974@mail.ru>
2021-11-09 16:35:50 +0300

    HelpMeTaskFinalWrong

--- task_final/solution.js
+++ task_final/solution.js
@@ -34,20 +34,23 @@
 // }
 
 function sendRequest(name, phone, address, goods, sum) {
-    let data = {goods: [], order: {}};
-
+    let data = {client: {name, phone} , order: {address, sum}, goods: [
+        {title: "Чебурек", count: "1"},
+        {title: "Пицца", count: "2"}
+    ]};
+           
     let countOfGoods = goods.length;
-
+    
     for (let i = 0; i <= countOfGoods; i += 1) {
         data.goods.push(goods[i].title);
     }
-
+    
     data.order.address = address;
     data.order.sum = name + phone + address + goods + sum;
-
+    
     data.client = 'Иван';
 
-    let jsonData = JSON.stringify(data);
+    let jsonData = JSON.stringify({data});
 
     return jsonData;
 }
