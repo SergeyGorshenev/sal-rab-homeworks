98db22e8d4eff0afb6ab7914f6b7880c5d9a8667
Sergey Gorshenev <gors1974@mail.ru>
2021-11-10 18:22:06 +0300

    HelpMeTaskFinalWrong - 2

--- task_final/solution.js
+++ task_final/solution.js
@@ -35,21 +35,21 @@
 
 function sendRequest(name, phone, address, goods, sum) {
     let data = {client: {name, phone} , order: {address, sum}, goods: [
-        {title: "Чебурек", count: "1"},
-        {title: "Пицца", count: "2"}
+        {title: "Чебурек", count: 1},
+        {title: "Пицца", count: 2}
     ]};
            
     let countOfGoods = goods.length;
     
     for (let i = 0; i <= countOfGoods; i += 1) {
-        data.goods.push(goods[i].title);
+        data.goods.push({title: goods[i].title, count: goods[i].count});
     }
     
-    data.order.address = address;
-    data.order.sum = name + phone + address + goods + sum;
+    data.client = name + " " + phone;
+    data.order.address = "ул." + street + ", дом" + house + ", подъезд" + entrance + ", этаж" + floor + ", кв", flat;
+    data.order.sum = sum;
+    data.goods = goods[i];
     
-    data.client = 'Иван';
-
     let jsonData = JSON.stringify({data});
 
     return jsonData;
