a8c0a14a34f86275140a318065aa9f8b47be9739
Sergey Gorshenev <gors1974@mail.ru>
2021-11-13 19:10:50 +0300

    Task final done

--- task_final/solution.js
+++ task_final/solution.js
@@ -34,25 +34,18 @@
 // }
 
 function sendRequest(name, phone, address, goods, sum) {
-    let data = {client: {name, phone},
-                order: {address: {street, house, entrance, floor, flat}, sum},
-                goods: [
-                    {title: "Чебурек", count: 1},
-                    {title: "Пицца", count: 2}
-    ]};
+    let data = {order: {}, goods: []};
            
     let countOfGoods = goods.length;
     
     for (let i = 0; i < countOfGoods; i += 1) {
         data.goods.push({title: goods[i].title, count: goods[i].count});
     }
-    
-    data.client = name + " " + phone;
-    data.order.address = "ул." + address.street + ", дом" + address.hous + ", подъезд" + address.entrance + ", этаж" + address.floor + ", кв" + address.flat;
+        
+    data.order.address = "ул. " + address.street + ", дом " + address.house + ", " + address.entrance + " подъезд, " + address.floor + " этаж, кв " + address.flat;
     data.order.sum = sum;
-    data.goods.title = goods.title;
-    data.goods.count = goods.count;
-    
+    data.client = name + " " + phone;
+
     let jsonData = JSON.stringify({data});
 
     return jsonData;
